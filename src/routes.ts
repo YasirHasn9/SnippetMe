e1485282357d37136732e5287877341eb7c34217
Yasir <hasanyasir01@gmail.com>
2023-06-06 09:22:35 -0500

    wip: setup lint

--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Express, Response } from "express";
-
-function routes(app: Express) {
-  // HEALTH CHECK
-  app.get("/ping", (_, res: Response) => {
-    res.status(200).send({ msg: "Pong" });
-  });
-}
-export default routes;
